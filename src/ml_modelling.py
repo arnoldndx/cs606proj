7eaf83ac8d635916ac8dba1662b25c55e25e51fa
Arnold <31108551+arnollim@users.noreply.github.com>
2022-03-05 15:55:36 +0800

    Update ml_modelling.py

--- src/ml_modelling.py
+++ src/ml_modelling.py
@@ -19,7 +19,7 @@ from pathlib import Path
 import sys
 sys.path.append('./ALNS')
 
-
+#Arnol
 ### draw and output solution ###
 def save_output(YourName, evrp, suffix):
     '''Draw the EVRP instance and save the solution
